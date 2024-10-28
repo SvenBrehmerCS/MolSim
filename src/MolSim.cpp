544d4ea8a8bb87dad44756d45cecee95e5e9884f
PatrickMetscher <patrick.metscher@gmx.de>
2024-10-28 23:07:06 +0100

    Removed blank comments.

--- src/MolSim.cpp
+++ src/MolSim.cpp
@@ -104,37 +104,3 @@ int main(const int argc, const char* argv[]) {
     std::cout << "output written. Terminating..." << std::endl;
     return 0;
 }
-
-// void calculateF(ParticleContainer& container, const Environment& env) {
-//     // Set the old f to f and reset the current forces
-//     for (Particle& p : container.get_particles()) {
-//         p.setOldF(p.getF());
-//         p.setF({ 0.0, 0.0, 0.0 });
-//     }
-
-//     for (size_t i = 0; i < container.get_particles().size(); i++) {
-//         for (size_t j = i + 1; j < container.get_particles().size(); j++) {
-//             // Calculate the distance and force experienced by two particles
-//             const double distance = ArrayUtils::L2Norm(container.get_particles()[j].getX() - container.get_particles()[i].getX());
-//             const double force = container.get_particles()[i].getM() * container.get_particles()[j].getM() / (distance * distance * distance);
-
-//             // Update the forces for both particles
-//             container.get_particles()[i].setF(
-//                 force * (container.get_particles()[j].getX() - container.get_particles()[i].getX()) + container.get_particles()[i].getF());
-//             container.get_particles()[j].setF(
-//                 force * (container.get_particles()[i].getX() - container.get_particles()[j].getX()) + container.get_particles()[j].getF());
-//         }
-//     }
-// }
-
-// void calculateX(ParticleContainer& container, const Environment& env) {
-//     for (Particle& p : container.get_particles()) {
-//         p.setX(p.getX() + env.get_delta_t() * p.getV() + (env.get_delta_t() * env.get_delta_t() * 0.5 / p.getM()) * p.getF());
-//     }
-// }
-
-// void calculateV(ParticleContainer& container, const Environment& env) {
-//     for (Particle& p : container.get_particles()) {
-//         p.setV(p.getV() + (env.get_delta_t() * 0.5 / p.getM()) * (p.getOldF() + p.getF()));
-//     }
-// }
--- src/MolSim.h
+++ src/MolSim.h
@@ -9,27 +9,3 @@
 
 #include "Environment.h"
 #include "ParticleContainer.h"
-
-// /**
-//  * Update the forces and old forces experienced by all the particles.
-//  *
-//  * @param container The container for the particles.
-//  * @param env The simulation environment.
-//  */
-// void calculateF(ParticleContainer& container, const Environment& env);
-
-// /**
-//  * Update the position of all the particles.
-//  *
-//  * @param container The container for the particles.
-//  * @param env The simulation environment.
-//  */
-// void calculateX(ParticleContainer& container, const Environment& env);
-
-// /**
-//  * Update the velocity of all the particles.
-//  *
-//  * @param container The container for the particles.
-//  * @param env The simulation environment.
-//  */
-// void calculateV(ParticleContainer& container, const Environment& env);
