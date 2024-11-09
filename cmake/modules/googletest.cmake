567c8ced75c9d2097896a3962f449a9ec62758be
Sven Brehmer(ge38kut) <sbre@outlook.de>
2024-11-09 13:45:00 +0100

    Put DOWNLOAD_EXTRACT_TIMESTAMP TRUE before the URL to support older cmake versions

--- cmake/modules/googletest.cmake
+++ cmake/modules/googletest.cmake
@@ -4,8 +4,8 @@ include(FetchContent)
 
 FetchContent_Declare(
         googletest
-        URL https://github.com/google/googletest/archive/b514bdc898e2951020cbdca1304b75f5950d1f59.zip
         DOWNLOAD_EXTRACT_TIMESTAMP TRUE
+        URL https://github.com/google/googletest/archive/b514bdc898e2951020cbdca1304b75f5950d1f59.zip
 )
 
 FetchContent_MakeAvailable(googletest)
