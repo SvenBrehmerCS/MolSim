e7fa7ed756470d3772c04d2cc5d5d6c7b4e21608
PatrickMetscher <patrick.metscher@gmx.de>
2024-11-11 21:29:41 +0100

    Updated the lenard jones calculator to match the new particle container class.

--- src/physicsCalculator/LJCalculator.cpp
+++ src/physicsCalculator/LJCalculator.cpp
@@ -23,10 +23,10 @@ namespace physicsCalculator {
     LJCalculator::~LJCalculator() = default;
 
     void LJCalculator::calculateF() {
-        for (size_t i = 0; i < container.get_particles().size(); i++) {
-            for (size_t j = i + 1; j < container.get_particles().size(); j++) {
+        for (auto i = container.begin(); i < container.end(); i++) {
+            for (auto j = i + 1; j < container.end(); j++) {
                 // Calculate the distance and force experienced by two particles
-                const double distance = ArrayUtils::L2Norm(container.get_particles()[j].getX() - container.get_particles()[i].getX());
+                const double distance = ArrayUtils::L2Norm(j->getX() - i->getX());
                 // Calculate the powers of (sigma / distance)
                 const double term = (env.get_sigma() / distance);
                 const double term_to_2 = term * term;
@@ -34,10 +34,8 @@ namespace physicsCalculator {
                 const double force = -24.0 * env.get_epsilon() / (distance * distance * distance) * (term_to_6 * term_to_6 - term_to_6);
 
                 // Update the forces for both particles
-                container.get_particles()[i].setF(
-                    force * (container.get_particles()[j].getX() - container.get_particles()[i].getX()) + container.get_particles()[i].getF());
-                container.get_particles()[j].setF(
-                    force * (container.get_particles()[i].getX() - container.get_particles()[j].getX()) + container.get_particles()[j].getF());
+                i->setF(force * (j->getX() - i->getX()) + i->getF());
+                j->setF(force * (i->getX() - j->getX()) + j->getF());
             }
         }
 
