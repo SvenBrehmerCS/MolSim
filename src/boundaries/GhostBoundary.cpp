97a6c061fc593bdcbb37cc01a9be0ddb220a4e94
PatrickMetscher <patrick.metscher@gmx.de>
2024-12-02 12:54:54 +0100

    Fixed a bug in the ghost reflection.

--- src/boundaries/GhostBoundary.cpp
+++ src/boundaries/GhostBoundary.cpp
@@ -10,10 +10,18 @@ void GhostBoundary::postF(Particle& particle, physicsCalculator::Calculator& cal
     const double r = calc.get_env().get_sigma() * scale * 0.5;
 
     // Create ghost particles if required
-    if (pos - particle.getX()[dim] < r) {
-        std::array<double, 3> f = { 0.0, 0.0, 0.0 };
-        f[dim] = calc.calculateFDist(particle.getX()[dim] - pos);
-        particle.setF(particle.getF() + f);
+    if (pos == 0.0) {
+        if (particle.getX()[dim] < r) {
+            std::array<double, 3> f = { 0.0, 0.0, 0.0 };
+            f[dim] = -calc.calculateFDist(particle.getX()[dim] * 2.0);
+            particle.setF(particle.getF() + f);
+        }
+    } else {
+        if (particle.getX()[dim] > pos - r) {
+            std::array<double, 3> f = { 0.0, 0.0, 0.0 };
+            f[dim] = calc.calculateFDist((pos - particle.getX()[dim]) * 2.0);
+            particle.setF(particle.getF() + f);
+        }
     }
 }
 
