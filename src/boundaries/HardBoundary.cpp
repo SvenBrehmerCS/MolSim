bdaf4be5e5c6bbbac587f8282fbab7e0dc1a5f10
PatrickMetscher <patrick.metscher@gmx.de>
2024-12-02 10:32:07 +0100

    Minor bug fixes in the hard reflective boundary.

--- src/boundaries/HardBoundary.cpp
+++ src/boundaries/HardBoundary.cpp
@@ -7,18 +7,18 @@ void HardBoundary::postF(Particle& particle, physicsCalculator::Calculator& calc
 
 void HardBoundary::postX(Particle& particle) {
     if (pos == 0.0) {
-        if (particle.getX()[dim] < 0) {
+        if (particle.getX()[dim] < pos) {
             std::array<double, 3> v = particle.getV();
-            v[dim] *= -1;
+            v[dim] *= -1.0;
             particle.setV(v);
             std::array<double, 3> x = particle.getX();
-            x[dim] *= -1;
+            x[dim] *= -1.0;
             particle.setV(x);
         }
     } else {
-        if (pos < particle.getX()[0]) {
+        if (particle.getX()[dim] > pos) {
             std::array<double, 3> v = particle.getV();
-            v[dim] *= -1;
+            v[dim] *= -1.0;
             particle.setV(v);
             std::array<double, 3> x = particle.getX();
             x[dim] = pos * 2.0 - particle.getX()[dim];
