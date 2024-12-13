5fe23023ae199f0c6dd259edb0facee7f5b654ce
PatrickMetscher <patrick.metscher@gmx.de>
2024-12-13 15:31:27 +0100

    Finished the type Descriptors.

--- src/container/TypeDesc.cpp
+++ src/container/TypeDesc.cpp
@@ -1,5 +1,10 @@
 #include "TypeDesc.h"
 
 TypeDesc::TypeDesc(const double m, const double s, const double e, const double delta_t, const double g) {
-    
+    mass = m;
+    sigma = s;
+    epsilon = e;
+    dt_m = delta_t * 0.5 / m;
+    dt_dt_m = delta_t * dt_m;
+    G = { 0.0, m * -g, 0.0 };
 }
