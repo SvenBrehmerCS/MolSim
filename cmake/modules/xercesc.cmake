c9d9701136cb4426377ce17de7f52389999857bf
PatrickMetscher <patrick.metscher@gmx.de>
2024-12-18 15:44:49 +0100

    Updated CmakeLists.

--- cmake/modules/xercesc.cmake
+++ cmake/modules/xercesc.cmake
@@ -7,3 +7,12 @@ FetchContent_Declare(
         GIT_REPOSITORY https://github.com/apache/xerces-c.git
         GIT_TAG v${XERCESC_VERSION}
 )
+
+FetchContent_MakeAvailable(xercesc)
+
+set(XercesC_INCLUDE_DIR ${xercesc_SOURCE_DIR}/src)
+set(XercesC_LIBRARY xerces-c)
+
+add_library(XercesC::XercesC INTERFACE IMPORTED)
+target_include_directories(XercesC::XercesC INTERFACE ${XercesC_INCLUDE_DIR})
+target_link_libraries(XercesC::XercesC INTERFACE xerces-c)
