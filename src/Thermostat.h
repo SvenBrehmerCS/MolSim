ea868c7bf19e0956e4b97222f127f617ad46e259
Sven Brehmer <sbre@outlook.de>
2024-12-19 22:43:16 +0100

    - Added missing Thermostat documentation - Removed troubling lines from the CMakeList

--- src/Thermostat.h
+++ src/Thermostat.h
@@ -56,23 +56,46 @@ public:
      */
     ~Thermostat() = default;
 
-    // TODO
-    // inline const bool get_dimensions() const { return dimensions; }
-
-    // inline const bool get_T_target() const { return T_target; }
-
-    // inline const bool get_max_change() const { return max_change; }
-
+    /**
+     * Get wether the thermostat is active.
+     *
+     * @return The activity status.
+     */
     inline const bool get_active() const { return active; }
 
+    /**
+     * Set the dimensions used for energie calculations.
+     *
+     * @param dimensions The dimensions of the simulation.
+     */
     inline void set_dimensions(int dimensions) { this->dimensions = dimensions; }
 
+    /**
+     * Set the temperature to regulate to.
+     *
+     * @param T_target The goal temperature.
+     */
     inline void set_T_target(double T_target) { this->T_target = T_target; }
 
+    /**
+     * Set the absolute maximum change of temperature per application.
+     *
+     * @param max_change The maximal temperature change.
+     */
     inline void set_max_change(double max_change) { this->max_change = max_change; }
 
+    /**
+     * Set the container the thermostat is regulating.
+     *
+     * @param particles The particle container representing the simulation.
+     */
     inline void set_particles(const std::shared_ptr<ParticleContainer>& particles) { this->particles = particles; }
 
+    /**
+     * Set the activity status of the thermostat.
+     *
+     * @param active The activity status.
+     */
     inline void set_active(bool active) { this->active = active; }
 
     /**
@@ -80,6 +103,11 @@ public:
      */
     void regulate_Temperature();
 
+    /**
+     * Converts the string into an easy to read string.
+     *
+     * @return String representation of the Thermostat.
+     */
     inline std::string toString() const {
         std::stringstream stream;
         stream << "Thermostat: dimensions: " << dimensions << " T_target: " << T_target << " max_change: " << max_change
@@ -87,12 +115,25 @@ public:
         return stream.str();
     }
 
+    /**
+     * An equal operator for testing purposes.
+     *
+     * @return Boolean representation of equality between the two thermostats.
+     */
     inline bool operator==(const Thermostat& other) const {
         return (dimensions == other.dimensions) && (T_target == other.T_target) && (max_change == other.max_change) && (particles == other.particles)
             && (active == other.active);
     }
 };
 
+/**
+ * Writes a textual representation of the thermostat to the stream.
+ *
+ * @param stream The stream to be written to.
+ * @param t The thermostat to be parsed.
+ *
+ * @return The stream with the textual representation added.
+ */
 inline std::ostream& operator<<(std::ostream& stream, const Thermostat& t) {
     stream << t.toString();
     return stream;
